30276d23dca1c330cd4a429c192dc1094317fece
Jagdish <jagdishsal@gmail.com>
2024-04-04 17:05:10 +0530

    home page

--- zoom-clone/app/(root)/(home)/page.tsx
+++ zoom-clone/app/(root)/(home)/page.tsx
@@ -1,4 +1,11 @@
+import MeetingTypeList from "@/components/MeetingTypeList";
+
 const Home = () => {
+    const now = new Date();
+    const time = now.toLocaleTimeString('IST', { hour: '2-digit', minute: '2-digit'});
+    const date = (new Intl.DateTimeFormat('IST', {
+        dateStyle: 'full'
+    })).format(now);
     return(
         <section className='flex size-full flex-col gap-10 text-white'>
             <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
@@ -8,12 +15,14 @@ const Home = () => {
                     </h2>
                     <div className='flex flex-col gap-2'>
                         <h1 className='text-4xl font-extrabold lg:text-7x-l'>
-                            11:30 AM
+                            {time}
                         </h1>
-                        <p className='text-lg font-medium text-sky-1 lg:text-2xl'>Thursday, April, 2024</p>
+                        <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
                     </div>
                 </div>
             </div>
+
+            <MeetingTypeList/>
         </section>
     );
 }
