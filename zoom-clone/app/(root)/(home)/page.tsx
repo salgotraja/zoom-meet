dab1be23806146b84dc4aad93b3e785dccb46b05
Jagdish <jagdishsal@gmail.com>
2024-04-06 10:16:37 +0530

    Join meeting feature

--- zoom-clone/app/(root)/(home)/page.tsx
+++ zoom-clone/app/(root)/(home)/page.tsx
@@ -1,4 +1,6 @@
 import MeetingTypeList from "@/components/MeetingTypeList";
+import Upcoming from "@/app/(root)/(home)/upcoming/page";
+import UpcomingMeeting from "@/components/UpcomingMeeting";
 
 const Home = () => {
     const now = new Date();
@@ -6,13 +8,12 @@ const Home = () => {
     const date = (new Intl.DateTimeFormat('IST', {
         dateStyle: 'full'
     })).format(now);
+
     return(
         <section className='flex size-full flex-col gap-10 text-white'>
             <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
                 <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-                    <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
-                        Upcoming Meeting at: 12:30 PM
-                    </h2>
+                    <UpcomingMeeting />
                     <div className='flex flex-col gap-2'>
                         <h1 className='text-4xl font-extrabold lg:text-7x-l'>
                             {time}
---
+++ zoom-clone/components/UpcomingMeeting.tsx
@@ -0,0 +1,32 @@
+//@ts-nocheck
+
+'use client';
+
+import { Call } from "@stream-io/video-react-sdk";
+import { useGetCalls } from "@/hooks/useGetCalls";
+import type { NextPage } from 'next'
+
+interface UpcomingMeetingProps {
+    latestCall?: Call;
+}
+
+const UpcomingMeeting: NextPage<UpcomingMeetingProps> = () => {
+    const { upcomingCalls } = useGetCalls();
+    const latestCall = upcomingCalls?.find((call, index, arr) =>
+        index === arr.length - 1
+    );
+
+    return (
+        <div>
+            {latestCall ? (
+                <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+                    Upcoming Meeting at: {latestCall.state?.startsAt.toLocaleString('IST', { hour: '2-digit', minute: '2-digit'}) || latestCall.start_time.toLocaleString('IST', { hour: '2-digit', minute: '2-digit'})}
+                </h2>
+            ) : (
+                <h1>No meeting scheduled for today</h1>
+            )}
+        </div>
+    );
+};
+
+export default UpcomingMeeting;
